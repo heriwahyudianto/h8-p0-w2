30541c30172a0aa41e9f1a402e95babe6d6545b4
heri wahyudianto <heriwahyudianto@gmail.com>
2020-02-05 09:20:15 +0700

    Berlatih Perulangan di JavaScript

--- exercise-6.js
+++ exercise-6.js
@@ -26,15 +26,29 @@ for (i=20; i>=1; i--) {
 //contoh - ganjil genap
 for(i=1; i <= 100; i++) {
   if (i%2 === 0) {
-    console.log(`Counter sekarang = ${i}, GENAP`);
+    console.log(`GENAP`);
   } else {
-    console.log(`Counter sekarang = ${i}, GANJIL`);
+    console.log(`GANJIL`);
   }
 }
 
 //contoh - untuk pertambahan counter 2
-for(i=1; i <= 100; i++) {
+for(i=1; i <= 100; i+=2) {
   if (i%3 === 0) {
     console.log(`${i} kelipatan 3`);
+  }
+}
+
+//contoh - untuk pertambahan counter 5
+for(i=1; i <= 100; i+=5) {
+  if (i%6 === 0) {
+    console.log(`${i} KELIPATAN 6`);
+  }
+}
+
+//counter sekarang = 10,
+for(i=1; i <= 100; i+=9) {
+  if (i%10 === 0) {
+    console.log(`${i} KELIPATAN 10`);
   }
 }
